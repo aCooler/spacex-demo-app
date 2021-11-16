29838e286eaa43b917b8613104adafffcf7199db
aCooler <asformyself@gmail.com>
2021-11-16 03:16:09 +0200

    [apollo] add fragments,client in thread,unit test

--- app/src/test/java/com/example/spacex_demo_app/ExampleUnitTest.kt
+++ app/src/test/java/com/example/spacex_demo_app/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package com.example.spacex_demo_app
 
+import android.os.Handler
+import android.os.Message
+import com.example.spacex_demo_app.api.Client
 import org.junit.Test
 
 import org.junit.Assert.*
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,17 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `test_is_true_if_everything_went_fine`(){
+        val handler: Handler = object : Handler() {
+            override fun handleMessage(msg: Message) {
+               assert(msg.data.containsKey("launch"))
+            }
+        }
+        val client = Client(handler)
+        client.initialise()
     }
+
 }
