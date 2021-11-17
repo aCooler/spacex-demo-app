dadc8b8e4dd45f268df777d512ab40d10881d417
aCooler <asformyself@gmail.com>
2021-11-17 15:47:24 +0200

    [feature_rxjava] implemented interface for using api with rx

--- app/src/main/java/com/example/spacex_demo_app/MainActivity.kt
+++ app/src/main/java/com/example/spacex_demo_app/MainActivity.kt
@@ -1,39 +1,51 @@
 package com.example.spacex_demo_app
 
-import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.Message
-import android.util.Log
 import android.widget.TextView
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.ApolloQueryCall
 import com.apollographql.apollo.api.Response
-import com.apollographql.apollo.exception.ApolloException
 import com.example.spacex_demo_app.api.Client
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-import android.os.Handler as Handler
-import com.apollographql.apollo.ApolloCall as ApolloCall
+import com.example.spacex_demo_app.api.SpaceXApi
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.observers.DisposableObserver
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var disposable:Disposable? = null
+    private var disposable1:Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView: TextView = findViewById(R.id.launches)
-        val handler: Handler = object : Handler() {
-            override fun handleMessage(msg: Message) {
-                textView?.text = msg.data.getString("launch")
-            }
-        }
-        val client = Client(handler)
-        client.initialise()
 
+        val apolloClient =
+            ApolloClient.builder().serverUrl("https://api.spacex.land/graphql/").build()
+
+        disposable = SpaceXApi(apolloClient).getLaunches().subscribe{resp ->
+            textView.text = resp.data?.launches?.get(0)?.details?.take(100).toString()
+        }
+
+        disposable1 = SpaceXApi(apolloClient).getLaunchById("9").subscribe{resp ->
+            textView.text =
+                textView.text.toString() + resp.data?.launch?.details?.take(100).toString()
+        }
     }
 
 
+    override fun onDestroy() {
+        disposable?.dispose()
+        disposable1?.dispose()
+        super.onDestroy()
+    }
+
 }
+
 
 
