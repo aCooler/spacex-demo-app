d9667171496428d30ca37ff9db71a046378d7e2a
aCooler <asformyself@gmail.com>
2021-11-18 11:04:13 +0200

    [feature_rxjava] fixed url constant and add CompositeDisposable

--- app/src/main/java/com/example/spacex_demo_app/MainActivity.kt
+++ app/src/main/java/com/example/spacex_demo_app/MainActivity.kt
@@ -5,13 +5,12 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.apollographql.apollo.ApolloClient
 import com.example.spacex_demo_app.api.SpaceXApi
-import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 class MainActivity : AppCompatActivity() {
 
     private val lim: Int = 100
-    private var disposable: Disposable? = null
-    private var disposable1: Disposable? = null
+    private var disposable: CompositeDisposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,22 +18,22 @@ class MainActivity : AppCompatActivity() {
         val textView: TextView = findViewById(R.id.launches)
 
         val apolloClient =
-            ApolloClient.builder().serverUrl("https://api.spacex.land/graphql/").build()
+            ApolloClient.builder().serverUrl(BuildConfig.SPACEX_ENDPOINT).build()
         var text = ""
-        disposable = SpaceXApi(apolloClient).getLaunches().subscribe { resp ->
+        disposable = CompositeDisposable()
+        disposable?.add(SpaceXApi(apolloClient).getLaunches().subscribe { resp ->
             text += resp.data?.launches?.get(0)?.details?.take(lim)
             textView.text = text
-        }
+        })
 
-        disposable1 = SpaceXApi(apolloClient).getLaunchById("9").subscribe { resp ->
+        disposable?.add(SpaceXApi(apolloClient).getLaunchById("9").subscribe { resp ->
             text += resp.data?.launch?.details?.take(lim)
             textView.text = text
-        }
+        })
     }
 
     override fun onDestroy() {
         disposable?.dispose()
-        disposable1?.dispose()
         super.onDestroy()
     }
 }
