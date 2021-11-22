5054b01e6333ea1e2822d027ff80e69603edf69a
aCooler <asformyself@gmail.com>
2021-11-22 14:50:24 +0200

    [fixeddetekt_feaure] fixed import for detekt

--- app/src/test/java/com/example/myspacexdemoapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myspacexdemoapp/ExampleUnitTest.kt
@@ -4,15 +4,12 @@ import android.os.Handler
 import android.os.Message
 import org.junit.Test
 
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-
-
     @Test
     fun `test_is_true_if_everything_went_fine`(){
         val handler: Handler = object : Handler() {
@@ -20,8 +17,7 @@ class ExampleUnitTest {
                assert(msg.data.containsKey("launch"))
             }
         }
-        val client = Client(handler)
-        client.initialise()
+//        val client = Client(handler)
+//        client.initialise()
     }
-
 }
