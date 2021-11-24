36bad112fe4d04581b054894be1eb2f488af3c23
aCooler <asformyself@gmail.com>
2021-11-24 17:04:21 +0200

    [CNTR-6] second commit

--- app/src/main/java/com/example/myspacexdemoapp/ui/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/myspacexdemoapp/ui/RecyclerViewAdapter.kt
@@ -15,46 +15,46 @@ class RecyclerViewAdapter :
 
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val rocketName: TextView
-        val success: TextView
         val place: TextView
-        val missionName: TextView
-        val date: TextView
-        val picture: ImageView
-        val badge: ImageView
+//        val rocketName: TextView
+//        val success: TextView
+//        val missionName: TextView
+//        val date: TextView
+//        val picture: ImageView
+//        val badge: ImageView
 
         init {
-            rocketName = view.findViewById(R.id.rocketName)
-            place = view.findViewById(R.id.place)
-            missionName = view.findViewById(R.id.missionName)
-            date = view.findViewById(R.id.date)
-            success = view.findViewById(R.id.success)
-            picture = view.findViewById(R.id.picture)
-            badge = view.findViewById(R.id.badge)
+            place = view.findViewById(R.id.place1)
+//            rocketName = view.findViewById(R.id.rocketName)
+//            missionName = view.findViewById(R.id.missionName)
+//            date = view.findViewById(R.id.date)
+//            success = view.findViewById(R.id.success)
+//            picture = view.findViewById(R.id.picture)
+//            badge = view.findViewById(R.id.badge)
         }
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.launch_card, viewGroup, false)
+            .inflate(R.layout.card1, viewGroup, false)
 
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
-        viewHolder.rocketName.text = items[position].rocketName
         viewHolder.place.text = items[position].place
-        viewHolder.missionName.text = items[position].name
-        viewHolder.date.text = items[position].date
-        viewHolder.success.text = when (items[position].success) {
-            true -> {
-                viewHolder.success.highlightColor = Color.GREEN
-                "V success"}
-            else -> {
-                viewHolder.success.highlightColor = Color.RED
-                "x failed"}
-        }
+//        viewHolder.rocketName.text = items[position].rocketName
+//        viewHolder.missionName.text = items[position].name
+//        viewHolder.date.text = items[position].date
+//        viewHolder.success.text = when (items[position].success) {
+//            true -> {
+//                viewHolder.success.highlightColor = Color.GREEN
+//                "V success"}
+//            else -> {
+//                viewHolder.success.highlightColor = Color.RED
+//                "x failed"}
+//        }
 
     }
 
