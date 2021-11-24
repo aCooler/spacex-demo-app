3c4327052df3b9a0aeea95a5f6e9e2b77b744581
aCooler <asformyself@gmail.com>
2021-11-24 17:39:17 +0200

    [CNTR-5.1] add data class to LaunchesViewState.kt

--- app/src/main/java/com/example/myspacexdemoapp/ui/LaunchesViewState.kt
+++ app/src/main/java/com/example/myspacexdemoapp/ui/LaunchesViewState.kt
@@ -4,8 +4,8 @@ import com.apollographql.apollo.api.Response
 import com.example.spacexdemoapp.GetLaunchesQuery
 
 sealed class LaunchesViewState {
-    class Success(val model: Response<GetLaunchesQuery.Data>): LaunchesViewState()
-    class Error(val error: Throwable): LaunchesViewState()
+    data class Success(val model: Response<GetLaunchesQuery.Data>): LaunchesViewState()
+    data class Error(val error: Throwable): LaunchesViewState()
     class Loading: LaunchesViewState()
 }
 
