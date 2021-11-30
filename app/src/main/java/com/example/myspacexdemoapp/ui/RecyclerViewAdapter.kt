f83d69ad9094d5f5fcb4c84568024a921d5ede4e
aCooler <asformyself@gmail.com>
2021-11-30 10:31:21 +0200

    [CNTR-6] fixed detekt issues

--- app/src/main/java/com/example/myspacexdemoapp/ui/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/myspacexdemoapp/ui/RecyclerViewAdapter.kt
@@ -10,10 +10,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-
 import com.example.myspacexdemoapp.R
 import com.example.myspacexdemoapp.api.toDateString
-
 
 class RecyclerViewAdapter :
     RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
