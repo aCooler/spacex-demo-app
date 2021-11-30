d8641a1766003c02ba7e602d0967ff17f1ff1706
aCooler <asformyself@gmail.com>
2021-11-30 13:44:54 +0200

    [CNTR-7] klint fix

--- app/src/main/java/com/example/myspacexdemoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/myspacexdemoapp/ui/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myspacexdemoapp.ui
 
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -8,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.myspacexdemoapp.R
-import com.example.myspacexdemoapp.ui.launch.DetailActivity
 import com.example.myspacexdemoapp.ui.launches.MainFragment
 
 class MainActivity : AppCompatActivity() {
