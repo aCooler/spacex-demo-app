3070c5c38d20508e8989711033513b9e18dae734
aCooler <asformyself@gmail.com>
2021-12-07 18:24:51 +0200

    [CNTR-4] changed test to apolloclient usage, compared values with captor

--- app/src/test/java/com/example/myspacexdemoapp/api/SpaceXApiTest.kt
+++ app/src/test/java/com/example/myspacexdemoapp/api/SpaceXApiTest.kt
@@ -1,30 +1,35 @@
 package com.example.myspacexdemoapp.api
 
 import com.apollographql.apollo.ApolloClient
+import com.example.spacexdemoapp.GetLaunchQuery
+import com.example.spacexdemoapp.GetLaunchesQuery
 import junit.framework.TestCase
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
+import org.mockito.ArgumentCaptor
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class SpaceXApiTest : TestCase() {
-    private var apolloClient: ApolloClient = mock(ApolloClient::class.java)
-
-    @Mock
-    var spaceXApi: SpaceXApi = SpaceXApi(apolloClient)
-//codefreeze 24
+    private val apolloClient: ApolloClient = mock(ApolloClient::class.java)
     @Test
     fun getLaunches() {
-        spaceXApi.getLaunches()
-        verify(spaceXApi).getLaunches()
+        val queryCaptor: ArgumentCaptor<GetLaunchesQuery> =
+            ArgumentCaptor.forClass(GetLaunchesQuery::class.java)
+        val query = GetLaunchesQuery()
+        apolloClient.query(query)
+        verify(apolloClient).query(queryCaptor.capture())
+        assertEquals(query, queryCaptor.value)
     }
-
     @Test
     fun getLaunchById() {
-        spaceXApi.getLaunchById("9")
-        verify(spaceXApi).getLaunchById("9")
+        val queryCaptor: ArgumentCaptor<GetLaunchQuery> =
+            ArgumentCaptor.forClass(GetLaunchQuery::class.java)
+        val query = GetLaunchQuery("9")
+        apolloClient.query(query)
+        verify(apolloClient).query(queryCaptor.capture())
+        assertEquals(query, queryCaptor.value)
     }
 }
