88e22abdc9dd0f33e65cae42a169d9c322d7061f
aCooler <asformyself@gmail.com>
2021-12-15 17:13:31 +0200

    [CNTR-10] final detailstest

--- app/src/androidTest/java/com/example/myspacexdemoapp/ui/launch/DetailsFragmentTest.kt
+++ app/src/androidTest/java/com/example/myspacexdemoapp/ui/launch/DetailsFragmentTest.kt
@@ -1,33 +1,32 @@
 package com.example.myspacexdemoapp.ui.launch
 
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions.swipeDown
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.Response
 import com.example.myspacexdemoapp.R
 import com.example.myspacexdemoapp.api.SpaceXApi
+import com.example.myspacexdemoapp.ui.launch.DetailsFragmentTest.SwipeRefreshLayoutMatchers.isRefreshing
 import com.example.myspacexdemoapp.ui.launches.LaunchUiModel
 import com.example.myspacexdemoapp.ui.launches.LinkInfo
 import com.example.myspacexdemoapp.ui.launches.Mission
 import com.example.myspacexdemoapp.ui.launches.Payload
 import com.example.myspacexdemoapp.util.ViewModelUtil
-import com.example.spacexdemoapp.GetLaunchQuery
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkClass
@@ -36,7 +35,6 @@ import io.reactivex.rxjava3.core.Observable
 import junit.framework.TestCase.assertEquals
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -44,6 +42,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class DetailsFragmentTest {
+
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
     var apolloClient: ApolloClient = mockkClass(ApolloClient::class)
@@ -52,16 +51,29 @@ class DetailsFragmentTest {
 
     @Test
     fun when_error_retrieved_than_dialog_is_showed() {
-        //TODO state from livedata
+        val detailsFragment = DetailsFragment("9")
+        detailsFragment.viewModelFactory = ViewModelUtil.createFor(viewModel)
+        val liveData: MutableLiveData<LaunchDetailsViewState> = MutableLiveData()
+        every { viewModel.launchLiveData } returns liveData
+        val list = mutableListOf<LaunchDetailsViewState>()
+        val observer = mockk<Observer<LaunchDetailsViewState>>(relaxed = true)
+        viewModel.launchLiveData.observeForever(observer)
         launchFragmentInContainer {
-            DetailsFragment("9")
-        }.onFragment {
-            assertNotNull(it.alertDialog)
-            assert(it.alertDialog.isShowing)
-
+            detailsFragment
+        }.onFragment { fragment ->
+            fragment.viewModelFactory = ViewModelUtil.createFor(viewModel)
         }
-        onView(withText("Error")).check(matches(ViewMatchers.isDisplayed()))
+        liveData.postValue(
+            LaunchDetailsViewState.Error(error = Throwable(message = "Test for empty list"))
+        )
+        verify(exactly = 1) { observer.onChanged(capture(list)) }
+        onView(withId(R.id.toolbar_details)).check(matches(ViewMatchers.isDisplayed()))
+        val state = list.last() as LaunchDetailsViewState.Error
+        onView(withText(state.error.message)).check { view, e ->
+            matches(ViewMatchers.isDisplayed())
+        }
     }
+
 
     fun atPosition(position: Int, itemMatcher: Matcher<View?>): Matcher<View?> {
         checkNotNull(itemMatcher)
@@ -79,6 +91,46 @@ class DetailsFragmentTest {
         }
     }
 
+    @Test
+    fun when_loading_retrieved_than_progress_is_showed() {
+        val detailsFragment = DetailsFragment("9")
+        detailsFragment.viewModelFactory = ViewModelUtil.createFor(viewModel)
+        val liveData: MutableLiveData<LaunchDetailsViewState> = MutableLiveData()
+        every { viewModel.launchLiveData } returns liveData
+        val list = mutableListOf<LaunchDetailsViewState>()
+        val observer = mockk<Observer<LaunchDetailsViewState>>(relaxed = true)
+        viewModel.launchLiveData.observeForever(observer)
+        launchFragmentInContainer {
+            detailsFragment
+        }.onFragment { fragment ->
+            fragment.viewModelFactory = ViewModelUtil.createFor(viewModel)
+        }
+        liveData.postValue(
+            LaunchDetailsViewState.Loading
+        )
+        verify(exactly = 1) { observer.onChanged(capture(list)) }
+        assert(list.last() is LaunchDetailsViewState.Loading)
+        onView(withId(R.id.swipe_refresh_details)).check { view, e ->
+            matches(isRefreshing())
+        }
+    }
+
+
+    object SwipeRefreshLayoutMatchers {
+        fun isRefreshing(): Matcher<View> {
+            return object : BoundedMatcher<View, SwipeRefreshLayout>(
+                SwipeRefreshLayout::class.java
+            ) {
+                override fun describeTo(description: Description) {
+                    description.appendText("is refreshing")
+                }
+
+                override fun matchesSafely(view: SwipeRefreshLayout): Boolean {
+                    return view.isRefreshing
+                }
+            }
+        }
+    }
 
 
     fun withCustomConstraints(action: ViewAction, constraints: Matcher<View>): ViewAction? {
@@ -97,31 +149,10 @@ class DetailsFragmentTest {
         }
     }
 
-
     @Test
     fun when_success_retrieved_than_list_is_filled_and_each_item_is_checked() {
         val detailsFragment = DetailsFragment("9")
         detailsFragment.viewModelFactory = ViewModelUtil.createFor(viewModel)
-        val response = mockk<Response<GetLaunchQuery.Data>> {
-            every { data } returns mockk {
-                every { launch() } returns mockk {
-                    every { rocket() } returns mockk {
-                        every { fragments() } returns mockk {
-                            every { rocketFields() } returns mockk {
-                                every { rocket_name() } returns "AC"
-                            }
-                        }
-                    }
-                    every { details() } returns "My details"
-                    every { fragments() } returns mockk {
-                        every { missionDetails() } returns mockk {
-                            every { mission_name() } returns "My mission name"
-                        }
-                    }
-                }
-            }
-        }
-        every { spaceXApi.getLaunchById("9") } returns Observable.just(response)
         val liveData: MutableLiveData<LaunchDetailsViewState> = MutableLiveData()
         every { viewModel.launchLiveData } returns liveData
         val list = mutableListOf<LaunchDetailsViewState>()
@@ -131,8 +162,6 @@ class DetailsFragmentTest {
             detailsFragment
         }.onFragment { fragment ->
             fragment.viewModelFactory = ViewModelUtil.createFor(viewModel)
-
-
         }
         liveData.postValue(LaunchDetailsViewState.Loading)
         liveData.postValue(
@@ -150,8 +179,6 @@ class DetailsFragmentTest {
                 )
             )
         )
-//        onView(withId(R.id.swipe_refresh_details))
-//            .perform(withCustomConstraints(swipeDown(), isDisplayingAtLeast(85)))
         onView(withId(R.id.launches_details_list)).check(matches(ViewMatchers.isDisplayed()))
         verify(exactly = 2) { observer.onChanged(capture(list)) }
         val state = list.last() as LaunchDetailsViewState.Success
@@ -159,13 +186,50 @@ class DetailsFragmentTest {
             if (view !is RecyclerView) {
                 throw e!!
             }
+
             matches(atPosition(0, hasDescendant(withText(state.model.number))))
-//                    onView(withId(R.id.launches_details_list))
-//                        .check(matches(atPosition(0, hasDescendant(withText(state.model.number)))))
 
         }
-        //assert(list.last() is LaunchDetailsViewState.Success)
+        val title = state.model.mission.name
+        onView(withText(title)).check(matches(ViewMatchers.isDisplayed()))
 
+    }
+
+
+    @Test
+    fun when_success_retrieved_than_title_is_checked() {
+        val detailsFragment = DetailsFragment("9")
+        detailsFragment.viewModelFactory = ViewModelUtil.createFor(viewModel)
+        val liveData: MutableLiveData<LaunchDetailsViewState> = MutableLiveData()
+        every { viewModel.launchLiveData } returns liveData
+        val list = mutableListOf<LaunchDetailsViewState>()
+        val observer = mockk<Observer<LaunchDetailsViewState>>(relaxed = true)
+        viewModel.launchLiveData.observeForever(observer)
+        launchFragmentInContainer {
+            detailsFragment
+        }.onFragment { fragment ->
+            fragment.viewModelFactory = ViewModelUtil.createFor(viewModel)
+        }
+        liveData.postValue(
+            LaunchDetailsViewState.Success(
+                model = LaunchUiModel(
+                    "889",
+                    linkInfo = LinkInfo.EMPTY,
+                    payload = Payload.EMPTY,
+                    mission = Mission.EMPTY.copy(name = "Antares")
+                )
+            )
+        )
+        verify(exactly = 1) { observer.onChanged(capture(list)) }
+        val state = list.last() as LaunchDetailsViewState.Success
+        onView(withId(R.id.toolbar_details)).check { view, e ->
+            if (view !is Toolbar) {
+                throw e!!
+            }
+            val title = state.model.mission.name
+            matches(withText(title))
+            matches(hasDescendant(withText(title)))
+        }
     }
 
     @Test
@@ -200,11 +264,9 @@ class DetailsFragmentTest {
                 )
             )
         )
-        onView(withId(R.id.swipe_refresh_details))
-            .perform(withCustomConstraints(swipeDown(), isDisplayingAtLeast(85)))
         onView(withId(R.id.launches_details_list)).check(matches(ViewMatchers.isDisplayed()))
         verify(exactly = 2) { observer.onChanged(capture(list)) }
-        val state = list.last() as LaunchDetailsViewState.Success
+        assert(list.last() is LaunchDetailsViewState.Success)
         onView(withId(R.id.launches_details_list)).check { view, e ->
             if (view !is RecyclerView) {
                 throw e!!
@@ -213,6 +275,4 @@ class DetailsFragmentTest {
         }
         assert(list.last() is LaunchDetailsViewState.Success)
     }
-
-
 }
--- app/src/main/java/com/example/myspacexdemoapp/ui/launch/DetailsFragment.kt
+++ app/src/main/java/com/example/myspacexdemoapp/ui/launch/DetailsFragment.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,9 +22,10 @@ class DetailsFragment(private val launchId: String) : Fragment(R.layout.details_
 
     private lateinit var viewModel: LaunchDetailsViewModel
     lateinit var viewModelFactory: ViewModelProvider.Factory
-    lateinit var alertDialog: AlertDialog
+
     lateinit var apolloClient : ApolloClient
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
            apolloClient =  ApolloClient.builder().serverUrl(BuildConfig.SPACEX_ENDPOINT).build()
         viewModelFactory = LaunchesViewModelFactory(SpaceXApi(apolloClient))
         viewModel =
@@ -41,21 +43,20 @@ class DetailsFragment(private val launchId: String) : Fragment(R.layout.details_
             when (state) {
                 is LaunchDetailsViewState.Error -> {
                     Log.d("LaunchDetailsViewState", state.error.message ?: "empty message")
-                    alertDialog = AlertDialog.Builder(requireActivity())
-                        .setMessage(state.error.message)
+                    val alertDialog = AlertDialog.Builder(requireActivity())
                         .setTitle(getString(R.string.error))
                         .setPositiveButton(getString(R.string.ok)) { dialog, which ->
                             dialog.cancel()
                         }.create()
-                        alertDialog.show()
+                    alertDialog.setMessage(state.error.message)
+                    alertDialog.show()
                 }
                 is LaunchDetailsViewState.Success -> {
                     val dataset =
                         LaunchUIMapper(launchUiModel = state.model).launchUiModelToDataModel()
                     adapter.setItems(dataset)
-                    //activity?.title = state.model.mission.name
-/*                    var toolbar: Toolbar = requireActivity().findViewById(R.id.toolbar_details)
-                    toolbar.title = state.model.mission.name*/
+                    var toolbar: Toolbar = requireActivity().findViewById(R.id.toolbar_details)
+                    toolbar.title = state.model.mission.name
                     mySwipeRefreshLayout?.isRefreshing = false
                 }
                 is LaunchDetailsViewState.Loading -> {
