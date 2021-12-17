21d8872fae9acbbb7a520153aaa6cf043a7d7bc8
aCooler <asformyself@gmail.com>
2021-12-17 17:27:41 +0200

    [CNTR-12] dagger implemented

--- app/src/main/java/com/example/myspacexdemoapp/MyApp.kt
+++ app/src/main/java/com/example/myspacexdemoapp/MyApp.kt
@@ -2,10 +2,7 @@ package com.example.myspacexdemoapp
 
 import android.app.Application
 import com.example.myspacexdemoapp.di.AppComponent
-import com.example.myspacexdemoapp.di.AppModule
 import com.example.myspacexdemoapp.di.DaggerAppComponent
-import com.example.myspacexdemoapp.di.NetModule
-import com.example.myspacexdemoapp.di.ViewModelModule
 
 class MyApp : Application() {
     var appComponent: AppComponent? = null
@@ -14,10 +11,6 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         appComponent =
-            DaggerAppComponent.builder()
-                .appModule(AppModule(this))
-                .netModule(NetModule())
-                .viewModelModule(ViewModelModule())
-                .build()
+            DaggerAppComponent.factory().create(applicationContext)
     }
 }
