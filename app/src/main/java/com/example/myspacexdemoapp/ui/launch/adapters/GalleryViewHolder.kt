e7c1ea52dc3208e724337ac2777da86ed2505b71
aCooler <asformyself@gmail.com>
2022-01-11 16:17:36 +0200

    [CNTR-22] merged fix

--- app/src/main/java/com/example/myspacexdemoapp/ui/launch/adapters/GalleryViewHolder.kt
+++ app/src/main/java/com/example/myspacexdemoapp/ui/launch/adapters/GalleryViewHolder.kt
@@ -8,8 +8,6 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class GalleryViewHolder(binding: GalleryBinding) : RecyclerView.ViewHolder(binding.root) {
-class GalleryViewHolder(binding: GalleryBinding) :
-    RecyclerView.ViewHolder(binding.root) {
     private val pager: ViewPager2 = binding.viewPager
     private val tabLayout: TabLayout = binding.tabDots
     private val adapter = CategoryAdapter()
