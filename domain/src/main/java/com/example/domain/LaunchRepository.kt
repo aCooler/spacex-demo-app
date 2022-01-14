c9b292b51219bdd2ae29909e4529ee75ae0d4413
aCooler <asformyself@gmail.com>
2022-01-14 16:42:52 +0200

    [CNTR-22] implemented CNTR-22, create usecases launchdata model and interface

---
+++ domain/src/main/java/com/example/domain/LaunchRepository.kt
@@ -0,0 +1,8 @@
+package com.example.domain
+
+import io.reactivex.rxjava3.core.Flowable
+
+interface LaunchRepository {
+    fun getLaunches(): Flowable<List<LaunchData>>
+    fun getLaunchById(id: String, payloadId: String): Flowable<LaunchData>
+}
